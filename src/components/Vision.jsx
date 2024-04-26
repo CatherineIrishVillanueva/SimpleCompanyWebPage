e76a70400a83b83a2c27db90c6f9ef928a869a13
Catherine Irish Villanueva <165612878+CatherineIrishVillanueva@users.noreply.github.com>
2024-04-26 09:12:33 +0800

    Update Vision.jsx

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -9,7 +9,7 @@ const Vision = () => {
                 <p className='text-gray-700 font-bold text-3xl relative'>VISION
                         <span className="absolute bottom-0 left-1/2 top-9 transform -translate-x-1/2 w-[100px] h-1 bg-[#00df9a] rounded-full"></span>
                     </p>
-                    <p className='text-black mt-4 justify-center text-lg'>At Plutosophia, we envision a future where the digital landscape is secure and empowered by artificial intelligence. Our mission is to pioneer innovative solutions at the intersection of cybersecurity and AI, safeguarding businesses and individuals against evolving threats while harnessing the potential of AI to enhance efficiency, accuracy, and resilience. We aspire to be the trusted partner of choice, driving forward the boundaries of cybersecurity and AI to create a safer, more intelligent world for all.</p>
+                    <p className='text-black mt-4 justify-center text-lg' style={{textAlign: 'justify'}}>At Plutosophia, we envision a future where the digital landscape is secure and empowered by artificial intelligence. Our mission is to pioneer innovative solutions at the intersection of cybersecurity and AI, safeguarding businesses and individuals against evolving threats while harnessing the potential of AI to enhance efficiency, accuracy, and resilience. We aspire to be the trusted partner of choice, driving forward the boundaries of cybersecurity and AI to create a safer, more intelligent world for all.</p>
                 </div>
                 <img className='w-[500px] mx-auto my-4' src={Image6} alt='Image3' />
             </div>
